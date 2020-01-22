2a3296b9aa896086e0605753353bac653ccf7ae6
Chris Simpkins <git.simpkins@gmail.com>
2020-01-21 23:25:49 -0500

    add dump::syntax with support for binary dumps by syntax name

--- src/dump/mod.rs
+++ src/dump/mod.rs
@@ -11,21 +11,5 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-pub mod build;
-pub mod dump;
-pub mod paths;
-
-pub const VERSION: &str = env!("CARGO_PKG_VERSION");
-
-#[cfg(test)]
-mod tests {
-    use crate::VERSION;
-    use regex::Regex;
-
-    #[test]
-    fn test_library_version() {
-        let re = Regex::new(r"^\d{1,3}\.\d{1,3}\.\d{1,3}$").unwrap();
-        assert!(re.is_match(VERSION));
-    }
-}
+pub mod syntax;
+pub mod theme;
---
+++ src/dump/syntax.rs
@@ -0,0 +1,131 @@
+// Copyright 2020 Christopher Simpkins
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+use std::boxed::Box;
+use std::error::Error;
+
+use syntect::dumps::dump_to_file;
+use syntect::parsing::SyntaxSet;
+
+use crate::build::syntax::build_syntaxset_by_names;
+
+pub fn dump_syntaxset_to_binary(ss: &SyntaxSet, filepath: &str) -> Result<(), Box<dyn Error>> {
+    match dump_to_file(ss, filepath) {
+        Ok(_n) => Ok(()),
+        Err(err) => Err(err),
+    }
+}
+
+pub fn dump_syntaxset_to_binary_by_names<'a, I>(
+    names: I,
+    filepath: &str,
+) -> Result<(), Box<dyn Error>>
+where
+    I: IntoIterator<Item = &'a &'a str>,
+{
+    match build_syntaxset_by_names(names) {
+        Ok(n) => {
+            dump_syntaxset_to_binary(&n, filepath)?;
+            Ok(())
+        }
+        Err(err) => Err(Box::new(err)),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use tempfile::tempdir;
+
+    use syntect::dumps::from_dump_file;
+    use syntect::parsing::SyntaxSet;
+
+    use crate::build;
+    use crate::dump;
+
+    #[test]
+    fn test_dump_syntaxset_to_binary() {
+        let tmpdir = tempdir().unwrap();
+        let file_path = tmpdir.path().join("syntaxes.bin");
+        let ss = build::syntax::build_technicolor_syntaxset().unwrap();
+        dump::syntax::dump_syntaxset_to_binary(&ss, file_path.to_str().unwrap()).unwrap();
+        let ss_in: SyntaxSet = from_dump_file(&file_path).unwrap();
+        assert!(file_path.is_file());
+        assert_eq!(&ss_in.find_syntax_by_extension("ini").unwrap().name, "INI");
+        // cleanup
+        tmpdir.close().unwrap();
+    }
+
+    #[test]
+    fn test_dump_syntaxset_to_binary_with_names_vector() {
+        let tmpdir = tempdir().unwrap();
+        let file_path = tmpdir.path().join("syntaxes.bin");
+        let names = vec![&"INI", &"Kotlin"];
+        dump::syntax::dump_syntaxset_to_binary_by_names(names, file_path.to_str().unwrap())
+            .unwrap();
+        let ss_in: SyntaxSet = from_dump_file(&file_path).unwrap();
+        assert!(file_path.is_file());
+        assert_eq!(&ss_in.find_syntax_by_extension("ini").unwrap().name, "INI");
+        assert_eq!(
+            &ss_in.find_syntax_by_extension("kt").unwrap().name,
+            "Kotlin"
+        );
+        assert!(&ss_in.find_syntax_by_extension("swift").is_none());
+        // cleanup
+        tmpdir.close().unwrap();
+    }
+
+    #[test]
+    fn test_dump_syntaxset_to_binary_with_names_array() {
+        let tmpdir = tempdir().unwrap();
+        let file_path = tmpdir.path().join("syntaxes.bin");
+        let names = ["INI", "Kotlin"];
+        dump::syntax::dump_syntaxset_to_binary_by_names(&names, file_path.to_str().unwrap())
+            .unwrap();
+        let ss_in: SyntaxSet = from_dump_file(&file_path).unwrap();
+        assert!(file_path.is_file());
+        assert_eq!(&ss_in.find_syntax_by_extension("ini").unwrap().name, "INI");
+        assert_eq!(
+            &ss_in.find_syntax_by_extension("kt").unwrap().name,
+            "Kotlin"
+        );
+        assert!(&ss_in.find_syntax_by_extension("swift").is_none());
+        assert!(&ss_in.find_syntax_by_extension("ts").is_none());
+        // cleanup
+        tmpdir.close().unwrap();
+    }
+
+    #[test]
+    fn test_dump_syntaxset_to_binary_with_names_hashmap() {
+        let mut names = HashMap::new();
+        names.insert("INI", 1);
+        names.insert("Kotlin", 2);
+
+        let tmpdir = tempdir().unwrap();
+        let file_path = tmpdir.path().join("syntaxes.bin");
+        dump::syntax::dump_syntaxset_to_binary_by_names(names.keys(), file_path.to_str().unwrap())
+            .unwrap();
+        let ss_in: SyntaxSet = from_dump_file(&file_path).unwrap();
+        assert!(file_path.is_file());
+        assert_eq!(&ss_in.find_syntax_by_extension("ini").unwrap().name, "INI");
+        assert_eq!(
+            &ss_in.find_syntax_by_extension("kt").unwrap().name,
+            "Kotlin"
+        );
+        assert!(&ss_in.find_syntax_by_extension("swift").is_none());
+        assert!(&ss_in.find_syntax_by_extension("ts").is_none());
+        // cleanup
+        tmpdir.close().unwrap();
+    }
+}
